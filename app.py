3531345d4fd17fbb8fead40fac07301d46ac0b67
jj-russell <jjrussell438@gmail.com>
2026-08-03 12:18:51 +0100

    select playlist takes to track swipe page for corresponding playlist

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request, url_for, g
+from flask_session import Session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import spotipy.util as util
 import os
 from dotenv import load_dotenv
+from functools import wraps
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "super-secret-key"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+@app.before_request
+def load_user():
+    scope="user-library-read"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                            client_secret=CLIENT_SECRET,
+                                                            redirect_uri=REDIRECT_URI,
+                                                            scope=scope))
+    g.user = sp
 
 @app.route("/")
 def index():
@@ -17,68 +33,47 @@ def index():
 
 @app.route("/select_playlist")
 def select_playlist():
-    scope="user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                        client_secret=CLIENT_SECRET,
-                                                        redirect_uri=REDIRECT_URI,
-                                                        scope=scope))
-    playlists_info = sp.current_user_playlists()
+    playlists_info = g.user.current_user_playlists()
     playlists = []
 
     for playlist in playlists_info["items"]:
         playlists.append(playlist)
         name = playlist["name"]
         playlist_id = playlist["id"]
-        # print(name)
 
     print(playlists)
 
     return render_template("select_playlist.html", playlists=playlists)
 
-def generate_playlist_array():
-    scope="user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                        client_secret=CLIENT_SECRET,
-                                                        redirect_uri=REDIRECT_URI,
-                                                        scope=scope))
-    playlists = sp.current_user_playlists()
-    for playlist in playlists["items"]:
-        name = playlist["name"]
-        playlist_id = playlist["id"]
-        if name != "Music":
-            continue
+def generate_playlist_array(playlist_id):
+    playlists = g.user.current_user_playlists()
 
-        results = sp.playlist_items(playlist_id, 
-                                fields="items(track(id,name,artists(name))),next",
-                                limit=100)
-        
-        # limited to 100 tracks per search, so must use a while loop to get more than 100 tracks
-        tracks = []
-        while True:
-            for item in results["items"]:
-                if item["track"] is not None:  # Ignore deleted/unavailable tracks
-                    tracks.append(item["track"])
+    results = g.user.playlist_items(playlist_id, 
+                            fields="items(track(id,name,artists(name))),next",
+                            limit=100)
+    
+    # limited to 100 tracks per search, so must use a while loop to get more than 100 tracks
+    tracks = []
+    while True:
+        for item in results["items"]:
+            if item["track"] is not None:  # Ignore deleted/unavailable tracks
+                tracks.append(item["track"])
 
-            if results["next"]:
-                results = sp.next(results)
-            else:
-                break
+        if results["next"]:
+            results = g.user.next(results)
+        else:
+            break
 
-        return tracks
+    return tracks
      
 
-@app.route("/track_swipe")
-def track_swipe():
-    scope="user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                        client_secret=CLIENT_SECRET,
-                                                        redirect_uri=REDIRECT_URI,
-                                                        scope=scope))
-    tracks = generate_playlist_array()
+@app.route("/track_swipe/<playlist_id>")
+def track_swipe(playlist_id):
+    tracks = generate_playlist_array(playlist_id)
     
     print(f"Found {len(tracks)} tracks.")
     for track in tracks:
-        track = sp.track(track["id"]) # get track info
+        track = g.user.track(track["id"]) # get track info
         image_url = track["album"]["images"][0]["url"]
         print(image_url)
         break
